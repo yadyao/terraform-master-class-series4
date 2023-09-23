62955eb551def0bbb01fb546b2ce3ee611b696bf
yadyao <118616142+yadyao@users.noreply.github.com>
2023-09-23 14:19:50 -0500

    Update main.tf

--- 08 - Terraform backend/backend/main.tf
+++ 08 - Terraform backend/backend/main.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket" "backend" {
   count = var.create_vpc ? 1 : 0
 
-  bucket = "lower(bootcamp32-${random_integer.s3.result}-${var.name}"
+  bucket = lower("bootcamp32-${random_integer.s3.result}-${var.name}")
 
   tags = {
     Name        = "My bucket"
